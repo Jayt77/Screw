d8b9343519bf720fc166696754c36fb3be819535
Jayt <loticate@gmail.com>
2024-12-30 12:04:59 +0200

    Lobby System added

--- Source/Screw/Screw.Build.cs
+++ Source/Screw/Screw.Build.cs
@@ -8,7 +8,7 @@ public class Screw : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SteamCorePro" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
